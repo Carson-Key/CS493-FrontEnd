41ebefce5e737f11f1d76e49748a46a7002be1a2
Carson-Key <keyc@oregonstate.edu>
2021-01-11 14:29:01 -0800

    Setup react routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './assets/main.css'
+import { HashRouter as Router } from 'react-router-dom';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import './assets/main.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="h-screen flex flex-wrap content-center justify-center">
-      <div className="p-4">1</div>
-      <div className="p-4">2</div>
-      <div className="p-4">3</div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="h-screen flex flex-wrap content-center justify-center">
+            <div className="p-4">1</div>
+            <div className="p-4">2</div>
+            <div className="p-4">3</div>
+          </div>
+        </Route>
+        <Route path="/wrong">
+          <div className="h-screen flex flex-wrap content-center justify-center">
+            <div className="p-4">2</div>
+            <div className="p-4">3</div>
+            <div className="p-4">1</div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
