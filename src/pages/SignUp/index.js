0b772d652a78d7ef0b460585c02cea4a61e7de39
Carson-Key <keyc@oregonstate.edu>
2021-01-11 19:18:11 -0800

    SignUp will redirect to dashboard on account creation

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import TextField from '../../components/TextField';
 import Button from '../../components/Button';
+import { useHistory } from 'react-router-dom';
 import { firebaseErrorMsg } from '../../helpers/errorHandling.js';
 import firebase from '../../assets/firebase.config.js';
+import { AuthContext } from '../../helpers/AuthContext.js';
 
 const SignUp = () => {
+  const [state, dispatch] = useContext(AuthContext)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const history = useHistory()
 
   const signUp = () => {
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((user) => {
+      dispatch({type: 'SET_USER', payload: user})
+      dispatch({type: 'SET_AUTHSTATE_AUTH'})
+      history.push(`/dashboard`)
     })
     .catch((error) => {
       firebaseErrorMsg(error)
