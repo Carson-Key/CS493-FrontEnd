5701ce4a579f093cbb7a34d9a4c37e5f5574a934
Carson-Key <keyc@oregonstate.edu>
2021-01-11 19:36:34 -0800

    Add sign out button

--- src/pages/LoggedIn/index.js
+++ src/pages/LoggedIn/index.js
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import Container from '../../components/Container';
+import Button from '../../components/Button';
+import { AuthContext } from '../../helpers/AuthContext.js';
+
 const LoggedIn = () => {
+  const [state, dispatch] = useContext(AuthContext)
+  const history = useHistory()
+
+  const logOut = () => {
+    dispatch({type: 'SET_USER', payload: null})
+    dispatch({type: 'SET_AUTHSTATE_UNAUTH'})
+    history.push(`/`)
+  }
+
   return (
-    <div className="h-screen flex flex-wrap content-center justify-center">
-      <div className="p-4">3</div>
-      <div className="p-4">1</div>
-      <div className="p-4">2</div>
-    </div>
+    <Container className="flex flex-col flex-wrap content-center justify-center m-auto">
+      <p>User Details</p>
+      <Button onClick={logOut}>Log Out</Button>
+    </Container>
   );
 }
 
