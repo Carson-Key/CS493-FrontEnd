6b6de3c1dc41ba80be355b0d22de864bc1bebcac
Carson-Key <keyc@oregonstate.edu>
2021-01-11 21:20:04 -0800

    Add new css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router } from 'react-router-dom';
 import Auth from './components/AuthHandler';
 import './assets/main.css';
+import './assets/misc.css';
 
 const App = () => {
   return (
