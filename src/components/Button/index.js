4b668f7f1103554d668165e042b5467c5a20343f
Carson-Key <keyc@oregonstate.edu>
2021-01-11 17:14:58 -0800

    Add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+const Button = (props) => {
+  const { onClick, children, className } = props
+
+  return (
+    <button type="button" onClick={onClick} className={className}>
+      {children}
+    </button>
+  )
+}
+
+export default Button;
