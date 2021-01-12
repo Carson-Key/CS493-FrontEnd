a900a8d41748b38561ef2d54034fd7821a84dbbe
Carson-Key <keyc@oregonstate.edu>
2021-01-11 19:09:30 -0800

    Add functionailiy to PrivateRoute

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,12 +1,30 @@
-import { Route } from 'react-router-dom';
+import { useEffect, useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../../helpers/AuthContext.js'
+import { authStates } from '../../helpers/authHelpers.js';
 
 const PrivateRoute = (props) => {
+  const [state, dispatch] = useContext(AuthContext)
   const { path, children } = props
 
+  useEffect(() => {
+    console.log(state.user)
+    console.log(state.authState)
+  }, [state.user, state.authState])
+
   return (
-    <Route path={path}>
-      {children}
-    </Route>
+    <Route
+      path={path}
+      render={() =>
+        state.user && state.authState === authStates.authorized ? (
+          children
+        ) : (
+          <Redirect
+            to="/"
+          />
+        )
+      }
+    />
   );
 }
 
