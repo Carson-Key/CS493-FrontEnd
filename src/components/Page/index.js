654b03e121f9dcceff72818a3725ad8ba076a088
Carson-Key <keyc@oregonstate.edu>
2021-01-11 19:31:33 -0800

    Screen now covers whole screen

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,7 +2,7 @@ const Page = (props) => {
   const { children } = props
 
   return (
-    <div className="bg-gray-400">
+    <div className="bg-gray-400 h-screen">
       {children}
     </div>
   )
