1ce0dc3ac15654a74150359b01ea5e88d87e9618
Carson-Key <keyc@oregonstate.edu>
2021-03-18 11:56:25 -0700

    Frontend now sends firebase token to backend

--- src/pages/LoggedIn/index.js
+++ src/pages/LoggedIn/index.js
@@ -5,6 +5,7 @@ import Button from '../../components/Button';
 import { AuthContext } from '../../helpers/AuthContext.js';
 import { ArtistContext } from '../../helpers/ArtistContext.js';
 import Page from '../../components/Page';
+import firebase from '../../assets/firebase.config.js';
 
 const LoggedIn = () => {
   const [artistsArray, setArtistsArray] = useState([1])
@@ -24,32 +25,36 @@ const LoggedIn = () => {
 
   useEffect(() => {
     if (!artistState.artists) {
-      fetch("https://q2h6cilfdi.execute-api.us-west-2.amazonaws.com/dev/artistList", {
-        headers: {
-          authorization: "hello",
-        }
-      })
-      .then((response) => {
-        response.json().then((data) => {
-          const dataArray = Object.keys(data.artists)
-          dataArray.forEach((artist) => {
-            const artistArray = Object.keys(data.artists[artist])
-            const artistObject = data.artists[artist]
-            artistArray.forEach((album) => {
-              const albumArray = Object.keys(artistObject[album])
-              const albumObject = artistObject[album]
-              albumArray.forEach((song) => {
-                albumObject[song].songObject = new Audio(albumObject[song].url)
+      firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+        fetch("https://q2h6cilfdi.execute-api.us-west-2.amazonaws.com/dev/artistList", {
+          headers: {
+            authorization: idToken,
+          }
+        })
+        .then((response) => {
+          response.json().then((data) => {
+            const dataArray = Object.keys(data.artists)
+            dataArray.forEach((artist) => {
+              const artistArray = Object.keys(data.artists[artist])
+              const artistObject = data.artists[artist]
+              artistArray.forEach((album) => {
+                const albumArray = Object.keys(artistObject[album])
+                const albumObject = artistObject[album]
+                albumArray.forEach((song) => {
+                  albumObject[song].songObject = new Audio(albumObject[song].url)
+                })
               })
             })
+            artistDispatch({type: 'SET_ARTISTS', payload: data.artists})
+            setArtistsArray(Object.keys(data.artists))
           })
-          artistDispatch({type: 'SET_ARTISTS', payload: data.artists})
-          setArtistsArray(Object.keys(data.artists))
         })
-      })
-      .catch((error) => {
+        .catch((error) => {
+          console.log(error)
+        })
+      }).catch(function(error) {
         console.log(error)
-      })
+      });
     } else {
       setArtistsArray(Object.keys(artistState.artists))
     }
